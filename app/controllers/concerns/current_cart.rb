41265396ffc52abd024a2bfd333047fdfcd96525
Leandran <41592532+Leandran@users.noreply.github.com>
2022-03-25 13:13:20 +0200

    Add Cart partial

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,8 +3,8 @@ module CurrentCart
   private
 
     def set_cart 
-      @cart = Cart.find(session[:cart_id])  #starts by finding cart_id, if none will create a new cart
-    rescue ActiveRecord::RecordNotFound     #then store that cart_id inside the seesion
+      @cart = Cart.find(session[:cart_id])  #starts by finding cart_id, if it does not exist it will create a new cart
+      rescue ActiveRecord::RecordNotFound   #then store that cart_id inside the session
       @cart = Cart.create
       session[:cart_id] = @cart.id
     end
