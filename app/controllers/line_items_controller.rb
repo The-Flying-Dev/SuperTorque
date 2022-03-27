4bf5b42711805c61fe8248ce3610bee9aac2fa97
Leandran <41592532+Leandran@users.noreply.github.com>
2022-03-27 09:56:13 +0200

    Complete base project

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,5 @@
 class LineItemsController < ApplicationController
+  skip_before_action :authorize, only: :create
   include CurrentCart
   before_action :set_cart, only: [:create] #find or create cart for current session
   before_action :set_line_item, only: %i[ show edit update destroy ]
