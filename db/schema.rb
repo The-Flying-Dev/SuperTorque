1d1367995a30118ddf8be8087ac26e77f7f52951
Leandran <41592532+Leandran@users.noreply.github.com>
2022-05-31 02:03:48 +0200

    Reset Db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_212636) do
+ActiveRecord::Schema.define(version: 2022_05_29_142639) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -50,15 +50,16 @@ ActiveRecord::Schema.define(version: 2022_05_30_212636) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stripe_customer_id"
-    t.string "email"
-    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
